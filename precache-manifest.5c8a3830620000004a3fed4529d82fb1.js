086055e78722e6fcd4d6c760ae8e2e415ef6cac1
Chas Campbell <chas@chascampbell.com>
2020-09-21 13:55:39 -0400

    Updates

---
+++ precache-manifest.5c8a3830620000004a3fed4529d82fb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b198578d829bba1195e4f4349563720f",
+    "url": "/profolio/index.html"
+  },
+  {
+    "revision": "454cf6c5124e9beb57c6",
+    "url": "/profolio/static/css/main.515169b7.chunk.css"
+  },
+  {
+    "revision": "18bd92579f39714fd81a",
+    "url": "/profolio/static/js/2.6c95c226.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/profolio/static/js/2.6c95c226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "454cf6c5124e9beb57c6",
+    "url": "/profolio/static/js/main.ab682614.chunk.js"
+  },
+  {
+    "revision": "c90d671bc29a0d7af553",
+    "url": "/profolio/static/js/runtime-main.93d18e1c.js"
+  }
+]);
